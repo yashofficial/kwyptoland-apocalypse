a5f42c25656c4361268f334ab791567c0158b6a2
Yashwant Raghuwanshi <30427960+yashofficial@users.noreply.github.com>
2021-11-02 15:14:41 +0530

    Update system.js

--- public/js/system.js
+++ public/js/system.js
@@ -2,6 +2,15 @@ AOS.init({
 once: true,
 });
 
+jQuery(document).ready(function(){
+  jQuery('.autoplay').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+});
+
 // var upgradeTime;
 // var seconds;
 
