99aa05d34a8471c5ca2025fd75a88335adc9bf71
Mohsin <mohsin.abbasi@6degreesit.com>
2021-11-08 11:26:46 +0530

    Speed image changes

--- components/Content.js
+++ components/Content.js
@@ -46,7 +46,7 @@ Public sale 21st Nov 7pm est</p>
 
                 <div className='what-block' data-aos="flip-left" data-aos-delay="400">
                     <div className='image-block' style={{'background-color': '#FEFF2E'}}>
-                        <img alt="" src="img/speed.gif" />
+                        <img alt="" src="img/Speed.gif" />
                     </div>
                     <div className='line1'>Speed</div>
                     <div className='line2'>Minting begins 21st Nov.</div>
@@ -58,7 +58,7 @@ Public sale 21st Nov 7pm est</p>
                     <div className='image-block' style={{'background-color': '#FEFF2E'}}>
                         <img alt="" src="img/winner.gif" />
                     </div>
-                    
+
                     <div className='line1'>Winner</div>
                     <div className='line2'>Ether Footy – The world’s first<br />
                                         NFT football game.<br />
